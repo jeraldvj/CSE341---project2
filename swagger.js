35131e140a1e1e14ff49143801f15d725aa2fa3a
jeraldvj <jeraldvj@byui.edu>
2024-02-10 21:38:57 -0400

    Project 2 - second step completed

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'Portfolio Builder',
     description: 'This is an api server for a portfolio builder website.\n'
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: 'https://cse341-project2-0lcr.onrender.com',
+  schemes: ['https']
 };
 
 const outputFile = './swaggerDoc.json';
