614fa5d6c12317574a021fa9d2f40347720dc29a
jeraldvj <jeraldvj@byui.edu>
2024-02-10 21:52:25 -0400

    Project 2 - second step completed

--- swagger.js
+++ swagger.js
@@ -2,8 +2,8 @@ const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
-    title: 'Portfolio Builder',
-    description: 'This is an api server for a portfolio builder website.\n'
+    title: 'Doctor Portfolio Builder',
+    description: 'This is an api server for a doctor portfolio builder website.\n'
   },
   host: 'https://cse341-project2-0lcr.onrender.com',
   schemes: ['https']
