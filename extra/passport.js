91e3e541754d51bd0c808da52537d6968d2700ca
jeraldvj <jeraldvj@byui.edu>
2024-02-25 22:59:03 -0400

    Progress in MyProject2 in week 07

--- extra/passport.js
+++ extra/passport.js
@@ -1,15 +1,18 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const GOOGLE_CLIENT_ID = '698476493230-rgspr38bhj20ietjuegjv3q7hbsk7oqc.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: 'http://localhost:8080/auth/google/callback',
+      callbackURL: 'https://cse341-project2-0lcr.onrender.com/auth/google/callback',
       passReqToCallback : true
     },
     (request, accessToken, refreshToken, profile, done) => {
